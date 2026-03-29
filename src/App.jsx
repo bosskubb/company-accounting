920c9c2530ebc2f35ba8d2b95d83fcc2d4490a3a
bosskubb <missyoualwaysboss@gmail.com>
2026-03-29 20:29:05 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDnhf_dIE1T0LlTMklxye6VdBWkI8M4YIo",
+  authDomain: "mycompanyaccounting-60b76.firebaseapp.com",
+  projectId: "mycompanyaccounting-60b76",
+  storageBucket: "mycompanyaccounting-60b76.firebasestorage.app",
+  messagingSenderId: "676343469584",
+  appId: "1:676343469584:web:e808070dde8d928642dc95",
+  measurementId: "G-C1Z850MCEZ"
+};
